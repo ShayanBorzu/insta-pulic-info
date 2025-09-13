04ad334d8f976e7aef656410b589a4551ae78150
Borzu Shayan <BorzuShayan@gmail.com>
2025-09-13 14:54:49 +0330

    added a comment.

--- instaapi/scraper/urls.py
+++ instaapi/scraper/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('profile/<str:username>/', profile_view),
 ]
 
-# GET  http://localhost:8000/api/profile/private_user/?login_user=  &login_pass=@1234@5678  -- private profile: if there is login infos
+# GET  http://localhost:8000/api/profile/private_user/?login_user=username&login_pass=password  -- private profile: if there is login infos (doesn't work, yet.)
 # GET  http://localhost:8000/api/profile/natgeo -- public profile: without login info
