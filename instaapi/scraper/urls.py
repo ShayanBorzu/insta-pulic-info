3990a0b9a57e2d3109efe11cb311f234c55b19a3
Borzu Shayan <BorzuShayan@gmail.com>
2025-09-13 16:31:51 +0330

    deleted some code.

--- instaapi/scraper/urls.py
+++ instaapi/scraper/urls.py
@@ -3,9 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('profile/<str:username>/', profile_view),
-    path('ipapi/<str:ip>/', ipapi_view),
-    path('ipapi/', ipapi_view),
-    
+    path('ipapi/<str:ip>/', ipapi_view),    
 ]
 
 # GET  http://localhost:8000/api/profile/private_user/?login_user=username&login_pass=password  -- private profile: if there is login infos (doesn't work, yet.)
--- instaapi/scraper/views.py
+++ instaapi/scraper/views.py
@@ -3,15 +3,6 @@ import requests
 from .utils import get_instagram_profile
 
 
-# def get_client_ip(request):
-#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#     if x_forwarded_for:
-#         # May contain multiple IPs if behind multiple proxies
-#         ip = x_forwarded_for.split(',')[0].strip()
-#     else:
-#         ip = request.META.get('REMOTE_ADDR')
-#     return ip
-
 
 def profile_view(request, username):
     # Read credentials from GET or POST
@@ -31,11 +22,6 @@ def profile_view(request, username):
     return JsonResponse(data)
 
 def ipapi_view(request, ip=None):
-
-
-    try:
-        if ip == None:
-            ip = request.META.get('REMOTE_ADDR')
 
         response = requests.get(f"https://ipapi.co/{ip}/json/")
         response.raise_for_status()  # Raise an error for bad responses
