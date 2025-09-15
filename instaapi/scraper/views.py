b7ba7dc90edb00e9e658d4996a40cbc702939d73
Borzu Shayan <BorzuShayan@gmail.com>
2025-09-15 18:15:04 +0330

    base64 image

--- instaapi/scraper/views.py
+++ instaapi/scraper/views.py
@@ -1,25 +1,30 @@
 from django.http import JsonResponse
 import requests
 from .utils import get_instagram_profile
+import requests, base64
 
 
-
+def image_url_to_base64(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return base64.b64encode(response.content).decode('utf-8')
+    else:
+        raise Exception(f"Failed to fetch image: {response.status_code}")
 
 def profile_view(request, username):
-    # Read credentials from GET or POST
-    login_user = (
-        request.GET.get("login_user")
-        or request.POST.get("login_user")
-    )
-    login_pass = (
-        request.GET.get("login_pass")
-        or request.POST.get("login_pass")
-    )
+    login_user = request.GET.get("login_user") or request.POST.get("login_user")
+    login_pass = request.GET.get("login_pass") or request.POST.get("login_pass")
     
     data = get_instagram_profile(username, login_user, login_pass)
 
     if "error" in data:
         return JsonResponse(data, status=400)
+
+    # Add base64 version of profile picture
+    profile_pic_url = data.get("profile_pic_url")
+    if profile_pic_url:
+        data["profile_pic_base64"] = image_url_to_base64(profile_pic_url)
+
     return JsonResponse(data)
 
 def ipapi_view(request, ip=None):
